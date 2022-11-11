1724746947cc7de0e919bdee9f10ac83b12df875
zbtechph <66845323+zbtechph@users.noreply.github.com>
2022-11-11 18:13:46 +0800

    Update router.js
    use html5 web history api

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
@@ -13,5 +13,5 @@ const routes = [
 
 export default createRouter({
   routes,
-  history: createWebHashHistory()
+  history: createWebHistory()
 })
